2c785e98f0bf90a9928615f0392e339fd90b8098
sayco <sayco.only@gmail.com>
2023-01-17 01:51:11 +0200

    Use circleci env

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,11 +1,11 @@
-eb init udagram --region us-east-1 --platform node.js
-eb create --sample udagram-env
-eb use udagram-env
+eb init $APP_NAME --region $AWS_DEFAULT_REGION --platform node.js
+eb create --sample $APP_ENV
+eb use $APP_ENV
 #POSTGRES_HOST == AWS RDS DB url
-eb setenv POSTGRES_HOST=database-1.c46aqqkmszp6.us-east-1.rds.amazonaws.com \
-POSTGRES_USERNAME=postgres \
-POSTGRES_PASSWORD=postgres \
-POSTGRES_DB=udagram \
-DB_PORT=5432 \
-JWT_SECRET=ryxwqMxmpI5Bxta6VRJu
-eb deploy udagram-env
+eb setenv POSTGRES_HOST=$POSTGRES_HOST \
+POSTGRES_USERNAME=$POSTGRES_USERNAME \
+POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+POSTGRES_DB=$POSTGRES_DB \
+DB_PORT=$DB_PORT \
+JWT_SECRET=$JWT_SECRET
+eb deploy $APP_ENV
--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,3 @@
 #"deploy": "npm install -f && npm run build && chmod +x bin/deploy.sh && bin/deploy.sh",
 #aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --recursive --acl public-read ./www s3://udagram-shabana/
+aws s3 cp --recursive --acl public-read ./www s3://$BUCKET_NAME/
