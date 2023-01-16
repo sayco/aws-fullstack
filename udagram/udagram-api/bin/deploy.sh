0cf025ad2560356b7b795bc06c6ede475e8a7aba
sayco <sayco.only@gmail.com>
2023-01-17 00:01:43 +0200

    working on aws

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,3 +1,11 @@
-#    "deploy": "npm run build && eb list && eb use udagram-api-dev && eb deploy",
-
-
+eb init udagram --region us-east-1 --platform node.js
+eb create --sample udagram-env
+eb use udagram-env
+#POSTGRES_HOST == AWS RDS DB url
+eb setenv POSTGRES_HOST=database-1.c46aqqkmszp6.us-east-1.rds.amazonaws.com \
+POSTGRES_USERNAME=postgres \
+POSTGRES_PASSWORD=postgres \
+POSTGRES_DB=udagram \
+DB_PORT=5432 \
+JWT_SECRET=ryxwqMxmpI5Bxta6VRJu
+eb deploy udagram-env
