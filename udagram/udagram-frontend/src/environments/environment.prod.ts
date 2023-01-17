9c442237ce680c4792b951dbdc697c70a5f06c88
sayco <sayco.only@gmail.com>
2023-01-17 12:51:30 +0200

    update front end env url

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-env.eba-jzspbprz.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-env.eba-ahtna7ib.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
